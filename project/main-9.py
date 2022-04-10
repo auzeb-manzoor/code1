7e0f2a341b6d4406863464b635a4b4d78738c33a
auzeb-manzoor <86683490+auzeb-manzoor@users.noreply.github.com>
2022-04-10 13:07:12 -0700

    Adding rgb to hex function

--- project/main-9.py
+++ project/main-9.py
@@ -43,6 +43,9 @@ with open('colours.csv','r') as Colour_file:
  #File Formatting, creation of dictionary
    open_dict[colour_keys]=name_keys
 print(open_dict)
+# example: print(rgbtohex((255, 255, 255)))
 #assign hex code and then assign a corresponding hexdecimal to the integer.
-def rgb2hex():
+
+def rgbtohex(rgb):
+  return '%02x%02x%02x' % rgb
   
