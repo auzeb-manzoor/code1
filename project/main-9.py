bafb48a96b7349cf7c3366ee525362704a8cb879
aaqibw <102716720+aaqibw@users.noreply.github.com>
2022-04-10 12:48:04 -0700

    Add files via upload

---
+++ project/main-9.py
@@ -0,0 +1,48 @@
+#CMPT project Part1
+#By aaqib wani
+
+
+#Dictionary(empty)
+open_dict={}
+
+
+#creation of menu, The Main Porgram Loop
+
+w=1
+while w==1:
+    print("[1]Load Colour Files")
+    print("[2]Print All Colours")
+    print("[3]Select Colour")
+    print("[4]Find Closest Colour")
+    print("[5]Display Colour")
+    print("[0]Exit")
+    option=int(input("choose an option from the above list"))
+    if option==1:
+      print("option 1 has been called")
+    elif option==2:
+      print("option 2 has been called")
+    elif option==3:
+      print("option 3 has been called")
+    elif option==4:
+      print("option 4 has been called")
+    elif option==5:
+      print("option 5 has been called")
+    elif option==0:
+      print("program terminated.")
+      break   
+    else:
+      print("try a valid input")
+#Opening the csv file
+import csv
+with open('colours.csv','r') as Colour_file:
+  file= csv.reader(Colour_file)
+  for line in file:
+   colour_keys=tuple((line[1],line[2],line[3]))
+#taking RGB values and presenting them as Tuples.
+   name_keys=str((line[0],"hexdecimal"))
+ #File Formatting, creation of dictionary
+   open_dict[colour_keys]=name_keys
+print(open_dict)
+#assign hex code and then assign a corresponding hexdecimal to the integer.
+def rgb2hex():
+  
